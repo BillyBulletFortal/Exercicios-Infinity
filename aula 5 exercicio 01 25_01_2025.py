d6c2643f8c31ca6ac8e17addfb2a149432787712
BillyBulletFortal <jbneto386@gmail.com>
2025-01-25 15:02:12 -0300

    aula 5
    exercicios sobre funçoes, começando funcoes lambada que so serao importantes em um estagio mais avançado do curso

---
+++ aula 5 exercicio 01 25_01_2025.py
@@ -0,0 +1,48 @@
+centimeters = float(0)
+medida = float(0)
+
+def transform ():
+    medida = float(input("Digite o valor em metros: >>    "))
+    centimeters = medida * 100
+    return centimeters
+
+
+
+print(transform())
+
+
+
+# FAÇA UMA FUNÇÃO QUE RECEBE (OU PEDE) UM VALOR EM METROS E RETORNA ESSE VALOR EM CENTIMETROS.
+
+
+
+
+# def conversorMetrosCentimetros():
+#     metros = float(input("Digite o valor em metros: "))
+#     centimetros = metros * 100
+#     return f"O valor de {metros}m equilave a {centimetros}cm"
+#     # return centimetros
+
+# print(conversorMetrosCentimetros())
+
+
+
+
+# VERSÃO 2
+
+# def conversorMetrosCentimetros(valorEmMetros):
+#     return valorEmMetros * 100
+
+# metros = float(input("Digite o valor em metros: "))
+# print(conversorMetrosCentimetros(valorEmMetros=metros))
+
+
+
+
+# VERSÃO 3
+# def conversorMetrosCentimetros(metros):
+#     return metros * 100
+
+# metros = float(input("Digite o valor em metros: "))
+# print(conversorMetrosCentimetros(metros))
+
